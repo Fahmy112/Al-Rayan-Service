270a5bd67330fce3756d1a8a59e54e2019b3c052
Fahmy Ezzat <fahmyezzat112@gmail.com>
2025-08-26 14:39:56 +0300

    fix 1  some edits on side bar

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,9 @@ import DashboardSummary from "./DashboardSummary";
 
 export default function Home() {
   return (
-    <div style={{direction: "rtl", fontFamily: "Cairo, Arial", maxWidth: 800, margin: "32px auto", background: "#fff", padding: 24, borderRadius: 12}}>
-      <h1 style={{color: '#286090'}}>نظام إدارة مركز الصيانة</h1>
+    <div style={{direction: "rtl", fontFamily: "Cairo, Arial", maxWidth: 800, margin: "auto", background: "#fff", padding: 24, borderRadius: 12}}>
+      <h1 style={{color: '#286090'}}> Welcome To AlRayan Auto Service  </h1>
       <nav style={{marginBottom: 24}}>
-        <Link href="/add" style={{marginLeft: 20, color: '#286090'}}>إضافة طلب صيانة جديد</Link>
-        <Link href="/requests" style={{color: '#286090'}}>متابعة الطلبات الجارية</Link>
       </nav>
       <DashboardSummary />
     </div>
