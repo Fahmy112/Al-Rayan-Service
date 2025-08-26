35127327a5a8a84c4298e25da324de0c3b04df6d
Fahmy Ezzat <fahmyezzat112@gmail.com>
2025-08-26 20:49:12 +0300

    fix adding اقسام قطع الغيار

---
+++ scripts/add_spare_categories.js
@@ -0,0 +1,39 @@
+// Script to add spare categories to MongoDB
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI || '';
+const client = new MongoClient(uri);
+
+const categories = [
+  'زيت الماتور',
+  'زيت الفتيس',
+  'فلتر الهواء',
+  'قلب طلمبة البنزين',
+  'فلتر زيت',
+  'فلتر تكييف',
+  'فلتر زيت فتيس',
+  'ماء تبريد',
+  'بوجيهات',
+  'فلتر بنزين',
+  'حشو فلتر زيت'
+];
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db();
+    const collection = db.collection('spare_categories');
+    for (const name of categories) {
+      await collection.updateOne(
+        { name },
+        { $set: { name } },
+        { upsert: true }
+      );
+    }
+    console.log('تمت إضافة الأقسام بنجاح');
+  } finally {
+    await client.close();
+  }
+}
+
+run();
